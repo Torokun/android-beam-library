dea69b1bd799fe3715652cc03d413672b2331ea1
Kazuya Matsubara <dip.switch.et.al@gmail.com>
2013-02-09 14:06:16 +0900

    nullチェックを追加

--- AndroidBeamLib/src/com/toro/android/lib/androidbeam/BeamReceiveHelper.java
+++ AndroidBeamLib/src/com/toro/android/lib/androidbeam/BeamReceiveHelper.java
@@ -4,6 +4,7 @@ package com.toro.android.lib.androidbeam;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.os.Build;
 import android.os.Parcelable;
@@ -26,7 +27,11 @@ public class BeamReceiveHelper {
     }
 
     public static String getReceivedBeamString(Intent intent) {
-        return new String(getReceivedBeam(intent));
+        byte[] beam = getReceivedBeam(intent);
+        if(beam == null) {
+            return null;
+        }
+        return new String(beam);
     }
 
     public static byte[] getReceivedBeam(Intent intent) {
@@ -43,7 +48,28 @@ public class BeamReceiveHelper {
             return null;
         }
         Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if(rawMsgs == null) {
+            Log.d(TAG, "raw message is null");
+            return null;
+        }
         NdefMessage msg = (NdefMessage) rawMsgs[0];
-        return msg.getRecords()[0].getPayload();
+        if(msg == null) {
+            Log.d(TAG, "ndef message is null");
+            return null;
+        }
+        NdefRecord[] ndefRecords = msg.getRecords();
+        if(ndefRecords == null) {
+            Log.d(TAG, "ndefRecords is null");
+            return null;
+        } else if(ndefRecords.length < 1) {
+            Log.d(TAG, "ndefRecords length is 0");
+            return null;
+        }
+        NdefRecord firstNdefRecord = ndefRecords[0];
+        if(firstNdefRecord == null) {
+            Log.d(TAG, "firstNdefRecord is null");
+            return null;
+        }
+        return firstNdefRecord.getPayload();
     }
 }
